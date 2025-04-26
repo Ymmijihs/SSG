1f17061dc63de28e9639fdf4a7b3cd015e81248b
Ymmijihs <shijimmy@gmail.com>
2025-04-26 07:47:51 +0200

    LeafNode added

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,21 +1,35 @@
 class HTMLNode:
-    def __init__(self, tag=None, value=None, children=None, props=None):
-        self.tag = tag
-        self.value = value
-        self.children = children
-        self.props = props
+	def __init__(self, tag=None, value=None, children=None, props=None):
+		self.tag = tag
+		self.value = value
+		self.children = children
+		self.props = props
 
-    def to_html(self):
-        raise NotImplementedError("to_html method not implemented")
+	def to_html(self):
+		raise NotImplementedError("to_html method not implemented")
 
-    def props_to_html(self):
-        if self.props is None:
-            return ""
-        props_html = ""
-        for prop in self.props:
-            props_html += f' {prop}="{self.props[prop]}"'
-        return props_html
+	def props_to_html(self):
+		if self.props is None:
+			return ""
+		props_html = ""
+		for prop in self.props:
+			props_html += f' {prop}="{self.props[prop]}"'
+		return props_html
 
-    def __repr__(self):
-        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+	def __repr__(self):
+		return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+
+class LeafNode(HTMLNode):
+	def __init__(self, tag=None, value=None, props=None):
+		super().__init__(tag=tag, value=value, props=props)
+
+	def to_html(self):
+		if self.value is None:
+			raise ValueError
+		props_html = self.props_to_html()
+		if not self.tag:
+			return f"{self.value}"
+		else:
+			return f"<{self.tag}{props_html}>{self.value}</{self.tag}>"
 
