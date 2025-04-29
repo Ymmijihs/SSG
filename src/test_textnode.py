003db06c56c6d41ac886a1492730ff4d7fd93eac
Jeroen Vermeulen <shijimmy@gmail.com>
2025-04-29 15:39:27 +0200

    added textnode to htmlnode

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -30,6 +30,55 @@ class TestTextNode(unittest.TestCase):
             "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
         )
 
+def test_text(self):
+    node = TextNode("This is a text node", TextType.TEXT)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, None)
+    self.assertEqual(html_node.value, "This is a text node")
+
+def test_text_node_as_bold(self):
+    node = TextNode("This is bold text", TextType.BOLD)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, "b")
+    self.assertEqual(html_node.value, "This is bold text")
+
+def test_text_node_as_italic(self):
+    node = TextNode("This is italic text", TextType.ITALIC)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, "i")
+    self.assertEqual(html_node.value, "This is italic text")
+
+def test_text_node_as_code(self):
+    node = TextNode("This is a code snippet", TextType.CODE)
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, "code")
+    self.assertEqual(html_node.value, "This is a code snippet")
+
+def test_text_node_as_link(self):
+    node = TextNode("Click here", TextType.LINK)
+    # Assuming self.href is set in the class context, for example as "http://example.com"
+    self.href = "http://example.com"
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, "a")
+    self.assertEqual(html_node.value, "Click here")
+    self.assertEqual(html_node.props, {"href": "http://example.com"})  # Check href property
+
+def test_text_node_as_image(self):
+    node = TextNode("Image description", TextType.IMAGE)
+    # Assuming self.src and self.alt are set in the class context
+    self.src = "image.png"
+    self.alt = "An example image"
+    html_node = text_node_to_html_node(node)
+    self.assertEqual(html_node.tag, "img")
+    self.assertEqual(html_node.props, {"src": "image.png", "alt": "An example image"})  # Check props
+
+def test_text_node_unsupported_type(self):
+    node = TextNode("Unsupported text", TextType.UNSUPPORTED)  # Assuming UNSUPPORTED is a defined enum
+    with self.assertRaises(Exception) as context:
+        text_node_to_html_node(node)
+    self.assertEqual(str(context.exception), "Not a supported TextType")
+
+
 
 if __name__ == "__main__":
     unittest.main()
--- src/textnode.py
+++ src/textnode.py
@@ -1,27 +1,45 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
-    TEXT = "text"
-    BOLD = "bold"
-    ITALIC = "italic"
-    CODE = "code"
-    LINK = "link"
-    IMAGE = "image"
+	TEXT = "text"
+	BOLD = "bold"
+	ITALIC = "italic"
+	CODE = "code"
+	LINK = "link"
+	IMAGE = "image"
 
 
 class TextNode:
-    def __init__(self, text, text_type, url=None):
-        self.text = text
-        self.text_type = text_type
-        self.url = url
+	def __init__(self, text, text_type, url=None):
+		self.text = text
+		self.text_type = text_type
+		self.url = url
 
-    def __eq__(self, other):
-        return (
-            self.text_type == other.text_type
-            and self.text == other.text
-            and self.url == other.url
-        )
+	def __eq__(self, other):
+		return (
+			self.text_type == other.text_type
+			and self.text == other.text
+			and self.url == other.url
+			)
 
-    def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+	def __repr__(self):
+		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+	def text_node_to_html_node(text_node):
+		if self.text_type == TEXT:
+			return LeafNode(value=self.text)
+		if self.text_type == BOLD:
+			return LeafNode(tag="b", value=self.text)
+		if self.text_type == ITALIC:
+			return LeafNode(tag="i", value=self.text)
+		if self.text_type == CODE:
+			return LeafNode(tag="code", value=self.text)
+		if self.text_type == LINK:
+			return LeafNode(tag="a", value=self.text, props="href")
+		if self.text_type == IMAGE:
+			return LeafNode(tag="img",props=("src","alt"))
+		else:
+			raise Exception("Not a supported TextType")
+
